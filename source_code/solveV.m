0c302ea5870a27b9aeb34ed00525225154c1fc39
Taiyi Wang <taiyiwang@DN0a1e68ca.SUNet>
2022-08-02 21:28:48 -0700

    clean up comments

--- source_code/solveV.m
+++ source_code/solveV.m
@@ -4,7 +4,7 @@ function residual = solveV(V, tauLock,s, a, V0, eta, sigma, p)
 % solve for stress-strength residual in the quasi-static stress balance equation
 % Input:
 % V = scalar slip velocity
-% tauLock = scalr initial shear stress + shear stress change due to loading
+% tauLock = scalar initial shear stress + shear stress change due to loading
 % a = rate-and-state direct effect coefficient
 % V0 = scalar reference velocity
 % eta = scalar radiation damping parameter
